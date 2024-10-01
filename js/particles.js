cc57d53f14c1674b617d6559ce0186bcef7ed32e
A.Sathish Kumar <sathishkumar5061.sse@saveetha.com>
2024-10-01 11:37:28 +0530

    first commit

---
+++ js/particles.js
@@ -0,0 +1,99 @@
+const canvas = document.getElementById("particlesCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray = [];
+
+// Particle class
+class Particle {
+constructor(x, y) {
+  this.x = x;
+  this.y = y;
+  this.size = Math.random() * 5 + 1; // Size of particle
+  this.speedX = Math.random() * 2 - 1; // Random horizontal speed
+  this.speedY = Math.random() * 2 - 1; // Random vertical speed
+}
+
+update() {
+  this.x += this.speedX;
+  this.y += this.speedY;
+
+  // Bounce particles off the edges
+  if (this.x < 0 || this.x > canvas.width) {
+    this.speedX = -this.speedX;
+  }
+  if (this.y < 0 || this.y > canvas.height) {
+    this.speedY = -this.speedY;
+  }
+}
+
+draw() {
+  ctx.fillStyle = 'white';
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+}
+}
+
+// Create initial particles
+function initParticles() {
+particlesArray = [];
+for (let i = 0; i < 100; i++) {
+  const x = Math.random() * canvas.width;
+  const y = Math.random() * canvas.height;
+  particlesArray.push(new Particle(x, y));
+}
+}
+
+// Link particles together
+function connectParticles() {
+for (let i = 0; i < particlesArray.length; i++) {
+for (let j = i; j < particlesArray.length; j++) {
+  const dx = particlesArray[i].x - particlesArray[j].x;
+  const dy = particlesArray[i].y - particlesArray[j].y;
+  const distance = Math.sqrt(dx * dx + dy * dy);
+
+  if (distance < 100) { // Adjust this value to change the connection distance
+    ctx.strokeStyle = 'rgba(255, 255, 255, 1)';
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+    ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+    ctx.stroke();
+  }
+}
+}
+}
+
+// Add particles at the click position
+canvas.addEventListener('click', function(event) {
+const x = event.clientX;
+const y = event.clientY;
+for (let i = 0; i < 100; i++) {  // Number of particles to add per click
+particlesArray.push(new Particle(x, y));
+}
+});
+
+// Animate particles
+function animateParticles() {
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+particlesArray.forEach((particle) => {
+particle.update();
+particle.draw();
+});
+connectParticles();
+requestAnimationFrame(animateParticles);
+}
+
+initParticles();
+animateParticles();
+
+// Resize canvas when window is resized
+window.addEventListener('resize', function() {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+initParticles();
+});
